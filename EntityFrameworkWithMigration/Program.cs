811275071ca6dd81eda52ad31278f314266b75c2
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-03-19 15:12:16 +0200

    Add project files.

---
+++ EntityFrameworkWithMigration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EntityFrameworkWithMigration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AirplaneDbContext context = new AirplaneDbContext();
+
+            context.Clients.Add(new Client
+            {
+                Name = "Volodia",
+                Birthday = new DateTime(2006, 12, 1),
+                Email = "volodia@gmail.com"
+            });
+
+            context.SaveChanges();
+
+            foreach (var c in context.Clients)
+            {
+                Console.WriteLine($"Client {c.Name}  {c.Email}  {c.Birthday.ToShortDateString()}");
+            }
+        }
+    }
+
+
+}
