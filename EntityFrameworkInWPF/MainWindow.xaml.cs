5bdf9bb13483d3c9455027037af8290a4c035b99
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-04-02 17:59:01 +0300

    Create library data_access

---
+++ EntityFrameworkInWPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using EntityFrameworkWithMigration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EntityFrameworkInWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AirplaneDbContext context = new AirplaneDbContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = context.Flights.ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = context.Clients.ToList();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = context.Airplanes.ToList();
+        }
+    }
+}
